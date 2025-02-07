4b5b355bd5ebbfb7f71563937f10a6ba4f80abbc
niraj070707 <karandeniraj28@gmail.com>
2025-02-08 01:56:27 +0530

    Add NavBar

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <div>Dashboard</div>
+      <UserButton/>
+    </div>
+    
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
+import { Button } from "../components/ui/button";
+
 import Image from "next/image";
-// import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold mb-4">Welcome to my websit</h1>
-      {/* <Button>Click Me</Button> */}
-    </div>
+      <Button>Click Me</Button>
+    </div> 
   );
-} 
+}
